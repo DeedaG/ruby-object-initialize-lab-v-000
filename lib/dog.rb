e26ba6b4b84256316989942dbd1ab19db08a60b9
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-06 00:11:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,10 @@
 
 class Dog
 
-  def initialize (dogs_name, dog_breed( ))
+  def initialize (dogs_name, dog_breed)
    @name = (dogs_name)
    @breed = (dog_breed)
-   if @name = (dogs_name) && @breed = (  )
-     return "Mutt"
-   end
-
+ end
 
   def name=(dogs_name)
    @name = dogs_name
