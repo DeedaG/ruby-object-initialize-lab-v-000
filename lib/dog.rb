c2b0b6e165b2d712d2fa791d487dd8520f06b057
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-06 00:56:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,14 @@
 
 class Dog
 
-  def initialize (dogs_name, k="dog_breed")
+  def initialize (dogs_name, dog_breed= Mutt)
    @name = (dogs_name)
-   @breed = (dog_breed)
-   @kind = 
- end
+   @breed = breed
+    end
+     def name
+       @name
+    end 
+  end
 
   def name=(dogs_name)
    @name = dogs_name
@@ -15,19 +18,4 @@ class Dog
     @name
   end
 
- def breed=(dog_breed)
-   @breed = dog_breed
-   if dog_breed.blank? = true
-     then puts {"Mutt"}
-  end
- end
-
- def breed
-   @breed
-   "Mutt"
- end
-end
-
-fido = Dog.new("Fido", "Pug")
-
-fido = Dog.new("Fido")
+ 
