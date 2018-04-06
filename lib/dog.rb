85d52256d62866aaf4ce60d327600eab18064539
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-06 00:59:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,9 @@
 
 class Dog
 
-  def initialize (dogs_name, dog_breed= Mutt)
-   @name = (dogs_name)
-   @breed = breed
-    end
-     def name
-       @name
-    end
+  def initialize(name, breed = "Mut")
+    @name = name
+    @breed = breed
   end
-
-  def name=(dogs_name)
-   @name = dogs_name
-  end
-
-  def name
-    @name
-  end
+end
+fido = Dog.new("Fido")
