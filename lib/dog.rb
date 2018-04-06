8d8f69babcc7d204c3f7299e1442d83867ff5607
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-06 00:00:23 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,31 @@
+
+class Dog
+
+  def initialize (dogs_name, dog_breed)
+   @name = (dogs_name)
+   @breed = (dog_breed)
+   if @breed != (dog_breed)
+     return "Mutt"
+   end
+ end
+
+  def name=(dogs_name)
+   @name = dogs_name
+  end
+
+  def name
+    @name
+  end
+
+ def breed=(dog_breed)
+   @breed = dog_breed
+ end
+
+ def breed
+   @breed
+ end
+end
+
+fido = Dog.new("Fido", "Pug")
+
+fido = Dog.new("Fido", )
